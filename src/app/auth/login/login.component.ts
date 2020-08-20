acaab26c90f9ea28578acfcb29d7143ecafe14de
chengdongzhang0710 <chengdongzhang0710@outlook.com>
2020-08-20 12:40:20 -0400

    Add authentication to the application

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import { AuthService } from '../auth.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,13 +10,14 @@ import { NgForm } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() {
+  constructor(private authService: AuthService) {
   }
 
   ngOnInit(): void {
   }
 
   onSubmit(form: NgForm) {
+    this.authService.login({ email: form.value.email, password: form.value.password });
   }
 
 }
